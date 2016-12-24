36f7b4dfa188e4d083d825878c7aca0690e06656
andrewh (CHIP) <andrewtholt60@example>
2016-12-24 15:37:09 +0000

    Multiple files, and output to named file.

--- atlast/toChar.py
+++ atlast/toChar.py
@@ -3,16 +3,23 @@
 import sys, getopt
 
 def usage():
-    print "Usage: toChar.py"
+    print "Usage: toChar.py -i file ... -i file -o outfile"
+
+    print "\tConvert a list of files into an array of strings."
+    print 'NOTE: Embedded quotes are not dealt with.'
     sys.exit(0)
 
 def process(name):
     out=""
+    rvalue=""
+
     start=False
 
+    rvalue=rvalue +  "// " + name + "\n"
     with open(name) as f:
         for line in f:
             tmp=line.strip()
+#            l=t.replace('"', '\\"')
 
             if len(tmp) > 0:
                 if tmp[0] == ':':
@@ -21,18 +28,20 @@ def process(name):
                 elif tmp.find(';') > -1:
                     start=False
                     out=out + line
-                    print '\t"' + out.strip() + '"'
-                    out = ""
+                    rvalue=rvalue + '\t"' + out.strip() + '"\n'
 
                 elif tmp[0] == '\\':
                     pass
                 if start:
                     out=out + " " + tmp
+    return rvalue
 
 def main(argv):
     fileList=[]
+    output="/dev/tty"
+
     try:
-        opts, args = getopt.getopt(argv,"hi:",["help","input="])
+        opts, args = getopt.getopt(argv,"hi:o:",["help","input=","output="])
     except getopt.GetoptError:
         print 'test.py -i <inputfile> -o <outputfile>'
         sys.exit(2)
@@ -42,11 +51,16 @@ def main(argv):
             usage()
         elif opt in ("-i","--input"):
             fileList.append(arg)
+        elif opt in ("-o","--output"):
+            output=arg
 
-    print "char nvramrc[] ="
+    out = open( output,"w")
+    out.write( "char nvramrc[] =")
     for f in fileList:
-        process( f)
-    print ";"
+        l=process( f)
+        out.write( l)
+    out.write( ";\n")
+    out.close()
 
 
 if __name__ == "__main__":
